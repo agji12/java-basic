9504804f90afa1c51526ab3905285a1278ab4e98
agji12 <agji12@naver.com>
2024-09-14 22:20:03 +0900

    섹션 8. final 수강 완료

---
+++ src/final1/FieldInit.java
@@ -0,0 +1,9 @@
+package final1;
+
+public class FieldInit {
+
+	static final int CONST_VALUE = 10; //static final이 붙으면 변수명을 대문자로 적어야 함
+	final int value = 10;
+	
+	//생성자로 초기화 불가 -> 이미 값을 초기화해줬기 때문에
+}
